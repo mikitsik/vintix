3e4c3ab8fab1dd6c8a6e5bdd8dfe97fcca718dc7
Alex Mikitsik <mikitsik@gmail.com>
2024-06-22 10:18:15 +0200

    init commit

---
+++ js/slider.js
@@ -0,0 +1,37 @@
+function slider() {
+  let slider = document.querySelector(".slider"),
+    last = slider.lastElementChild,
+    first = slider.firstElementChild,
+    btn = document.querySelectorAll(".btn");
+
+  slider.insertBefore(last, first);
+
+  btn.forEach(btn => {
+    btn.addEventListener("click", movement);
+  });
+  setInterval(function()
+  {
+      movement({target:{id:"next"}});
+  }, 4000);
+  function movement(e) {
+    slider = document.querySelector(".slider");
+    last = slider.lastElementChild;
+    first = slider.firstElementChild;
+
+    const activeSlide = document.querySelector(".active");
+
+    if (e.target.id === "next") {
+      slider.insertBefore(first, last.nextSibling);
+
+      activeSlide.classList.remove("active");
+      activeSlide.nextElementSibling.classList.add("active");
+    } else {
+      slider.insertBefore(last, first);
+      activeSlide.classList.remove("active");
+      activeSlide.previousElementSibling.classList.add("active");
+    }
+  }
+}
+slider();
+
+
